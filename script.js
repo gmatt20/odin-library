139161d53888395097efd68951e0187f9fa18652
Matthew Garcia <gmatthew2765@gmail.com>
2024-11-05 09:44:21 -0800

    Working on JS logic.

--- script.js
+++ script.js
@@ -1,68 +1,72 @@
-/*
-function Book(title, author, pages, status) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.readBook = readBook;
-  this.info = () => {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readBook}`;
-  };
-}
-
-function AddBookToLibrary() {}
-
-const atomicHabits = new Book(
-  "Atomic Habits",
-  "James Clear",
-  100,
-  "finished reading"
-);
-
-console.log(atomicHabits.info());
-
-const blended = new Book(
-  "Blended",
-  "Sharon M. Draper",
-  320,
-  "reading it, on chapter 4."
-);
-
-console.log(blended.info());
-*/
-
 function Book(title, author, pages, status) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.status = status;
-  this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readBook}`;
-  };
 }
 
-// Book.prototype.info = function () {
-//   return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readBook}`;
-// };
+const myLibrary = [];
 
-const myLibrary = [
-  new Book("Atomic Habits", "James Clear", 100, "finished reading"),
-  new Book("Blended", "Sharon M. Draper", 320, "currently reading"),
-];
-
-// document.querySelector(".new-book-button").addEventListener("click", () => {
-//   const bookDisplayContainer = document.querySelector(
-//     ".book-display-container"
-//   );
-//   for (let i = 0; i < myLibrary.length; i++) {
-//     bookDisplayContainer.innerHTML = myLibrary[i].info();
-//   }
-// });
+const modalElement = document.querySelector(".modal");
+const form = document.getElementById("form");
 
 document.querySelector(".new-book-button").addEventListener("click", () => {
+  modalElement.style.display = "block";
+});
+
+form.addEventListener("submit", () => {
+  const formData = new FormData(form);
+
+  const title = formData.get("title");
+  const author = formData.get("author");
+  const pages = formData.get("pages");
+  const status = formData.get("status");
+
+  const myLibrary = [new Book(title, author, pages, status)];
+
+  DisplayBooks();
+});
+
+document.querySelector(".js-close-icon").addEventListener("click", () => {
+  modalElement.style.display = "none";
+});
+
+function DisplayBooks() {
+  console.log("worked");
   const bookDisplayContainer = document.querySelector(
     ".book-display-container"
   );
+  bookDisplayContainer.innerHTML += "";
   for (let i = 0; i < myLibrary.length; i++) {
-    bookDisplayContainer.innerHTML = myLibrary[i].info();
+    const bookInfoContainer = document.createElement("div");
+    bookInfoContainer.classList.add("book-info-container");
+
+    const bookHeader = document.createElement("div");
+    bookHeader.classList.add("book-header");
+
+    const bookName = document.createElement("p");
+    bookName.classList.add("book-name");
+    bookName.textContent = myLibrary[i].title;
+
+    const authorName = document.createElement("p");
+    authorName.classList.add("author-name");
+    authorName.textContent = `By ${myLibrary[i].author}`;
+
+    const pagesDiv = document.createElement("div");
+    pagesDiv.classList.add("pages");
+    pagesDiv.textContent = `Total pages: ${myLibrary[i].pages}`;
+
+    const statusDiv = document.createElement("div");
+    statusDiv.classList.add("status");
+    statusDiv.textContent = `Status: ${myLibrary[i].status}`;
+
+    bookHeader.appendChild(bookName);
+    bookHeader.appendChild(authorName);
+
+    bookInfoContainer.appendChild(bookHeader);
+    bookInfoContainer.appendChild(pagesDiv);
+    bookInfoContainer.appendChild(statusDiv);
+
+    bookDisplayContainer.appendChild(bookInfoContainer);
   }
-});
+}
